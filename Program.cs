3f53c9822077023519c281c0eb9c081c2fdbf2bc
damians20171 <48625171+damians20171@users.noreply.github.com>
2019-03-16 15:57:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor AOC = new Monitor(20, 30, 25.5);
+            Console.WriteLine(AOC.GetSpecsOfMonitor());
+            Console.ReadKey();
+        }
+    }
+}
+
+class Device
+{
+    public int height
+    {
+        get;
+        protected set;
+    }
+    public int width
+    {
+        get;
+        protected set;
+    }
+    public Device()
+    {
+        this.height = 0;
+        this.width = 0;
+
+    }
+    public Device(int height, int width)
+    {
+        this.height = height;
+        this.width = width;
+    }
+    public string GetSpecs()
+    {
+        return this.height + ", " + this.width;
+
+    }
+}
+
+class Monitor : Device
+{
+    public double inches
+    {
+        get;
+        protected set;
+
+    }
+    public Monitor(int height, int width, double inches) : base(height, width)
+    {
+        this.inches = inches;
+    }
+    public string GetSpecsOfMonitor()
+    {
+        return base.GetSpecs() + " " + this.inches;
+    }
+}
